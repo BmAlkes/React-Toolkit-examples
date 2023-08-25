362d170fa29941e5f76e2cb47b61040effd4063a
bmalkes <bmalkes@gmail.com>
2023-08-25 21:10:59 +0300

    feat:add last push

--- src/features/PostList.jsx
+++ src/features/PostList.jsx
@@ -2,18 +2,23 @@ import { useSelector } from "react-redux";
 import { selectAllPosts } from "./posts/postsSlice";
 import { PostAuthor } from "./posts/PostAuthor";
 import TimeAgo from "./posts/TimeAgo";
+import ReactionButtons from "./posts/Reactions";
 
 export const PostList = () => {
   const posts = useSelector(selectAllPosts);
-  console.log(posts);
 
-  const renderedPosts = posts.map((post) => (
+  const orderedPost = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderedPosts = orderedPost.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
       <p className="postCredit">
         <PostAuthor userId={post.userId} />
         <TimeAgo timestamp={post.date} />
+        <ReactionButtons post={post} />
       </p>
     </article>
   ));
